a84d81384b47b2255ad34599b37afd98ba7f9765
381601286@qq.com <381601286@qq.com>
2017-04-15 17:37:18 +0800

    offwork

--- jdbc/JDBC-Research/src/com/research/base/DemoConnection.java
+++ jdbc/JDBC-Research/src/com/research/base/DemoConnection.java
@@ -1,11 +1,57 @@
 package com.research.base;
 
+import java.io.FileInputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.Properties;
 
 public class DemoConnection {
+	private String driver;
+	private String url;
+	private String user;
+	private String pass;
+	Connection conn;
+	Statement stmt;
+	private PreparedStatement pstmt;
+	public void initParam(String paramFile)throws Exception{
+		Properties props = new Properties();
+		props.load(new FileInputStream(paramFile));
+		driver = props.getProperty("driver");
+		url = props.getProperty("url");
+		user = props.getProperty("user");
+		pass = props.getProperty("pass");
+	}
+	public void createTable(String sql)throws Exception{
+		try{
+			Class.forName(driver);
+			conn = DriverManager.getConnection(url,user,pass);
+			stmt = conn.createStatement();
+			pstmt = conn.prepareStatement(sql);
+			ResultSet rs = pstmt.executeQuery();
+			//ResultSet rs = stmt.executeQuery(sql);
+
+			 while(rs.next()) {   
+			       System.out.println(rs.getInt("ID")+","+rs.getString("NAME"));
+			      }
+		}finally{
+			if(stmt !=null){
+				stmt.close();
+			}
+			if (conn != null){
+				conn.close();
+			}
+		}
+	}
+
+	public static void main(String[] args) throws Exception{
+		DemoConnection dc = new DemoConnection();
+		dc.initParam("h2.ini");
+		dc.createTable("select * from test");
+	}
+	  /*
 	public static void main(String[] args) throws Exception{
 		Class.forName("org.h2.Driver");
 		   Connection conn = DriverManager.
@@ -17,7 +63,7 @@ public class DemoConnection {
 		       System.out.println(rs.getInt("ID")+","+rs.getString("NAME"));
 		      }
 		   conn.close();
-		   /*
+		 
 		String driver = "com.mysql.jdbc.Driver";
 		String url = "jdbc:mysql://127.0.0.1:3306/javadbtest";
 		String user = "root";
@@ -55,6 +101,6 @@ public class DemoConnection {
 			e.printStackTrace();
 		} catch (Exception e) {
 			e.printStackTrace();
-		}*/
-	}
+		}
+	}*/
 }
